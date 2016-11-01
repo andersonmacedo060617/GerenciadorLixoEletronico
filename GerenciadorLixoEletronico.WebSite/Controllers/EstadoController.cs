6d45a1d685099b606aa941af9cd6bd7976f74717
Anderson Macedo <dasilvamacedo.anderson@gmail.com>
2016-11-01 08:16:09 -0200

    - Construida controller de Pais - CRUD de Pais contruido

---
+++ GerenciadorLixoEletronico.WebSite/Controllers/EstadoController.cs
@@ -0,0 +1,19 @@
+﻿using GerenciadorLixoEletronico.NH.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciadorLixoEletronico.WebSite.Controllers
+{
+    public class EstadoController : Controller
+    {
+        // GET: Estado
+        public ActionResult Index()
+        {
+            var estados = ConfigDB.Instance.EstadoRepository.GetAll();
+            return View();
+        }
+    }
+}
---
+++ GerenciadorLixoEletronico.WebSite/Controllers/PaisController.cs
@@ -0,0 +1,72 @@
+﻿using GerenciadorLixoEletronico.NH.Config;
+using GerenciadorLixoEletronico.NH.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciadorLixoEletronico.WebSite.Controllers
+{
+    public class PaisController : Controller
+    {
+        // GET: Pais
+        public ActionResult Index()
+        {
+            var paises = ConfigDB.Instance.PaisRepository.GetAll();
+            return View(paises);
+        }
+
+        public ActionResult Alterar(int id)
+        {
+            var pais = ConfigDB.Instance.PaisRepository.GetAll().FirstOrDefault(x => x.Id == id);
+            if(pais != null)
+            {
+                return View(pais);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Gravar(Pais pais)
+        {
+            if (!ModelState.IsValid)
+            {
+                RedirectToAction("Alterar", pais);
+            }
+
+            ConfigDB.Instance.PaisRepository.Gravar(pais);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ConfirmaDelete(int id)
+        {
+            var pais = ConfigDB.Instance.PaisRepository.GetAll().FirstOrDefault(x => x.Id == id);
+            if(pais != null)
+            {
+                return View(pais);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            var pais = ConfigDB.Instance.PaisRepository.GetAll().FirstOrDefault(x => x.Id == id);
+            
+            if (pais != null)
+            {
+                ConfigDB.Instance.PaisRepository.Excluir(pais);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+    }
+}
